adb55e72971c65169e413dd7e9eaff2b69228815
MORE-E <more.e.developer@gmail.com>
2017-06-10 17:07:55 +0800

    [QuickProject]add QuickTest project with logger

---
+++ QuickTest/src/test/java/com/moree/TestCase1.java
@@ -0,0 +1,17 @@
+package com.moree;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by MORE-E on 6/10/2017.
+ */
+public class TestCase1 {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Test
+    public void test1() {
+        logger.info("hello world!");
+    }
+}
