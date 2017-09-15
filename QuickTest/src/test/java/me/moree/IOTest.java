f90798f4f2f35686d53a677741e3d9833cc47809
Li Tenghui <lith@silvrr.com>
2017-09-15 15:41:25 +0800

    增加文件IO用法

--- QuickTest/src/test/java/me/moree/IOTest.java
+++ QuickTest/src/test/java/me/moree/IOTest.java
@@ -13,9 +13,9 @@ public class IOTest {
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
     @Test
-    public void readFile() {
+    // getResourceAsStream()必须要有斜杠
+    public void readFileViaStream() {
         Class baseClass = this.getClass();
-        // 必须要有斜杠
         try (InputStream is = baseClass.getResourceAsStream("/logback-test.xml")) {
             if (is != null) {
                 char[] buffer = new char[4096];
@@ -30,5 +30,19 @@ public class IOTest {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    @Test
+    // 通过File读文件没有斜杠，跟ide的working directory有关，.代表当前项目目录，即QuickTest这个目录，也可以去掉./
+    public void readFileViaFileReader() throws IOException {
+    	File file = new File("pom.xml");
+    	BufferedReader reader = new BufferedReader(new FileReader(file));
+	    String line;
+	    StringBuilder builder = new StringBuilder();
+	    while ((line = reader.readLine()) != null) {
+	    	builder.append(line);
+	    }
+	    reader.close();
+	    logger.info(builder.toString());
     }
 }
