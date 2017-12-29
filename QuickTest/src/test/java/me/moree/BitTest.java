0e6c60380220fe67d54cb391ee9a7073d6fc135a
Li Tenghui <lith@silvrr.com>
2017-12-29 11:59:33 +0800

    add bitwise operation

---
+++ QuickTest/src/test/java/me/moree/BitTest.java
@@ -0,0 +1,57 @@
+package me.moree;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author MORE-E
+ * created on 2017-12-29
+ */
+public class BitTest {
+	private Logger logger = LoggerFactory.getLogger(getClass());
+
+	@Test
+	public void test1() {
+		int num = 0;
+		logger.debug(Integer.toBinaryString(num));
+		logger.debug(Integer.toBinaryString(setBit(num, 1)));
+		logger.debug(Integer.toString(setBit(num, 1)));
+		logger.debug(Integer.toBinaryString(setBit(num, 2)));
+		logger.debug(Integer.toString(setBit(num, 2)));
+		logger.debug(Integer.toBinaryString(setBit(num, 3)));
+		logger.debug(Integer.toString(setBit(num, 3)));
+		int num2 = 10;
+		logger.debug(Integer.toBinaryString(num2));
+		logger.debug(Integer.toBinaryString(clrBit(num2, 1)));
+		logger.debug(Integer.toString(clrBit(num2, 1)));
+		logger.debug(Boolean.toString(isTrue(num2, 1)));
+		logger.debug(Integer.toBinaryString(clrBit(num2, 2)));
+		logger.debug(Integer.toString(clrBit(num2, 2)));
+		logger.debug(Boolean.toString(isTrue(num2, 2)));
+		logger.debug(Integer.toBinaryString(clrBit(num2, 3)));
+		logger.debug(Integer.toString(clrBit(num2, 3)));
+		logger.debug(Boolean.toString(isTrue(num2, 3)));
+
+		int num3 = 5; // 101
+		int num4 = 3; // 011
+		Assert.assertEquals(diff(num3, num4), 6);
+	}
+
+	private int setBit(int num, int pos) {
+		return num | (1 << pos);
+	}
+
+	private int clrBit(int num, int pos) {
+		return num & ~(1 << pos);
+	}
+
+	private static boolean isTrue(int num, int pos) {
+		return ((num >> pos) & 1) == 1;
+	}
+
+	private static int diff(int num1, int num2) {
+		return num1 ^ num2;
+	}
+}
